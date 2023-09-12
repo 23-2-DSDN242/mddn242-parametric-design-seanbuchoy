c64b4ef759439f9fdb289cca54a075e5d2a0957b
seanbuchoy <seancajilog45@gmail.com>
2023-09-12 19:29:28 +1200

    barcode

--- draw_letters.js
+++ draw_letters.js
@@ -24,35 +24,37 @@ function drawLetter(letterData) {
   let posy = 55;
 
   // determine parameters for second circle
-  let size2 = letterData["size"];
+  let sizex1 = letterData["size"];
+  let sizey1 = letterData["size2"];
   let pos2x = 50  + letterData["offsetx"];
   let pos2y = 150 + letterData["offsety"];
 
-  scale(1);
+
   noStroke();
   fill(0);
   rect(posx, posy-50, 10, 190);
-  rect(posx+13, posy-50, 5, 190);
-  rect(posx+35, posy-50, 5, 190);
-  rect(posx+45, posy-50, 5, 190);
-  rect(posx+60, posy-50, 15, 190);
-  rect(posx+80, posy-50, 5, 190);
+  rect(posx+12, posy-50, 5, 190);
+  rect(posx+21, posy-50, 5, 190);
+  rect(posx+28, posy-50, 5, 190);
+  rect(posx+37, posy-50, 10, 190);
+  rect(posx+49, posy-50, 5, 190);
+  rect(posx+58, posy-50, 5, 190);
+  rect(posx+65, posy-50, 5, 190);
+  rect(posx+74, posy-50, 5, 190);
+  rect(posx+83, posy-50, 10, 190);
   rect(posx+95, posy-50, 5, 190);
-  rect(posx+110, posy-50, 5, 190);
-  rect(posx+125, posy-50, 5, 190);
-  rect(posx+140, posy-50, 15, 190);
-  rect(posx+160, posy-50, 5, 190);
 
   // draw two circles
   fill(255, 0, 0);
   rect(50, 150, 75, 120);
   fill(black);
-  rect(pos2x, pos2y, size2, size2);
+  rect(pos2x, pos2y, sizex1, sizey1);
 }
 
 function interpolate_letter(percent, oldObj, newObj) {
   let new_letter = {};
   new_letter["size"]    = map(percent, 0, 100, oldObj["size"], newObj["size"]);
+  new_letter["size2"]    = map(percent, 0, 100, oldObj["size2"], newObj["size2"]);
   new_letter["offsetx"] = map(percent, 0, 100, oldObj["offsetx"], newObj["offsetx"]);
   new_letter["offsety"] = map(percent, 0, 100, oldObj["offsety"], newObj["offsety"]);
   return new_letter;
--- editor.js
+++ editor.js
@@ -6,6 +6,7 @@
  */
 const sliderInfo = [
   ["size",       0, 100],
+  ["size2",       0, 100],
   ["offsetx",  -30,  30],
   ["offsety", -100, 100]
 ];
